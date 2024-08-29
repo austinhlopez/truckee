4446a0a284e2eecf5a508ae8a9d0cfc5c5625796
Your Guide al <alopez@codeceleste.com>
2024-08-29 00:26:17 -0400

    mix phx new

---
+++ test/truckee_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule TruckeeWeb.PageControllerTest do
+  use TruckeeWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
